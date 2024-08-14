c9596002a2a0efb3859fa9b05fdaaa2199f85e76
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-08-14 22:39:02 +0530

    Deployment

---
+++ src/main/java/com/Projects/SpringBoot/ConstraintViolationException/SpringBootConstraintViolationExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.Projects.SpringBoot.ConstraintViolationException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConstraintViolationExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConstraintViolationExceptionApplication.class, args);
+	}
+
+}
